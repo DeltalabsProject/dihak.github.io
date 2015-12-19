3558cfb88f782266c9092c443124ed48a7ecc8fa
Dihak <dihakoo@gmail.com>
2015-12-19 19:25:23 +0700

    Update mdblogz.js

--- mdblogz/mdblogz.js
+++ mdblogz/mdblogz.js
@@ -1,21 +1,3 @@
-var Script = function() {
-    jQuery('.sidebar .sub-menu > a').click(function() {
-        var last = jQuery('.sub-menu.open', $('.sidebar'));
-        last.removeClass("open");
-        jQuery('.arrow', last).removeClass("open");
-        jQuery('.collapse', last).slideUp(500);
-        var sub = jQuery(this).next();
-        if (sub.is(":visible")) {
-            jQuery('.arrow', jQuery(this)).removeClass("open");
-            jQuery(this).parent().removeClass("open");
-            sub.slideUp(500)
-        } else {
-            jQuery('.arrow', jQuery(this)).addClass("open");
-            jQuery(this).parent().addClass("open");
-            sub.slideDown(500)
-        }
-    })
-}();
 $('.post').each(function() {
     if ($(this).find('.post-label').length) {     
         var cl = $(this).find('.post-label').text();
