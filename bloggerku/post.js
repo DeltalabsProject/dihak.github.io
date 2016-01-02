2a39de6eb9bc33ba955686c376ceabaf307665d6
Dihak <dihakoo@gmail.com>
2016-01-02 22:25:07 +0700

    Create post.js

---
+++ bloggerku/post.js
@@ -0,0 +1,20 @@
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 N;5 n;5 i;5 v;1l();C j(a){5 b=A.15("E");b.1a="17/13";b.1b=a;A.1w.16(b)}C 1m(a){5 b=\'\';L=I(O/2);4(L==O-L){O=L*2+1}H=i-L;4(H<1)H=1;k=I(a/r)+1;4(k-1==a/r)k=k-1;K=H+O-1;4(K>k)K=k;b+="<3 6=\'1p\'>1z "+i+\' 1D \'+k+"</3>";5 c=I(i)-1;4(i>1){4(i==2){4(n=="u"){b+=\'<3 6="1v"><a 7="\'+D+\'">\'+X+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">\'+X+\'</a></3>\'}}9{4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+c+\');z B">\'+X+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+c+\');z B">\'+X+\'</a></3>\'}}}4(H>1){4(n=="u"){b+=\'<3 6="l"><a 7="\'+D+\'">1</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">1</a></3>\'}}1g(5 d=H;d<=K;d++){4(i==d){b+=\'<3 6="1B">\'+d+\'</3>\'}9 4(d==1){4(n=="u"){b+=\'<3 6="l"><a 7="\'+D+\'">1</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">1</a></3>\'}}9{4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+d+\');z B">\'+d+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+d+\');z B">\'+d+\'</a></3>\'}}}4(K<k){4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+k+\');z B">\'+k+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+k+\');z B">\'+k+\'</a></3>\'}}5 e=I(i)+1;4(i<k){4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+e+\');z B">\'+1f+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+e+\');z B">\'+1f+\'</a></3>\'}}5 f=A.1q("1t");5 g=A.1H("1x-1y");1g(5 p=0;p<f.P;p++){f[p].1e=b}4(f&&f.P>0){b=\'\'}4(g){g.1e=b}}C 11(a){5 b=a.1d;5 c=I(b.1r$1s.$t,10);1m(c)}C 1l(){5 a=s;4(a.m("/w/x/")!=-1){4(a.m("?U-h")!=-1){v=a.F(a.m("/w/x/")+14,a.m("?U-h"))}9{v=a.F(a.m("/w/x/")+14)}}4(a.m("?q=")==-1&&a.m(".1A")==-1){4(a.m("/w/x/")==-1){n="u";4(s.m("#G=")!=-1){i=s.F(s.m("#G=")+8,s.P)}9{i=1}j(D+"Z/T/Y?h-o=1&W=R-Q-E&V=11")}9{n="x";4(a.m("&h-o=")==-1){r=1u}4(s.m("#G=")!=-1){i=s.F(s.m("#G=")+8,s.P)}9{i=1}j(D+\'Z/T/Y/-/\'+v+\'?W=R-Q-E&V=11&h-o=1\')}}}C M(a){S=(a-1)*r;N=a;5 b=A.1h(\'1c\')[0];5 c=A.15(\'E\');c.1a=\'17/13\';c.1i("1b",D+"Z/T/Y?1j-1k="+S+"&h-o=1&W=R-Q-E&V=12");b.16(c)}C J(a){S=(a-1)*r;N=a;5 b=A.1h(\'1c\')[0];5 c=A.15(\'E\');c.1a=\'17/13\';c.1i("1b",D+"Z/T/Y/-/"+v+"?1j-1k="+S+"&h-o=1&W=R-Q-E&V=12");b.16(c)}C 12(a){18=a.1d.1C[0];5 b=18.1n.$t.F(0,19)+18.1n.$t.F(1E,1F);5 c=1G(b);4(n=="u"){5 d="/w?U-h="+c+"&h-o="+r+"#G="+N}9{5 d="/w/x/"+v+"?U-h="+c+"&h-o="+r+"#G="+N}1o.7=d}',62,106,'|||span|if|var|class|href||else||||||||max|nomerhal||maksimal|showpageNum|indexOf|jenis|results|||postperpage|urlactivepage||page|lblname1|search|label|onclick|return|document|false|function|home_page|script|substring|PageNo|mulai|parseInt|redirectlabel|akhir|nomerkiri|redirectpage|nopage|numshowpage|length|in|json|jsonstart|posts|updated|callback|alt|upPageWord|summary|feeds||hitungtotaldata|finddatepost|javascript||createElement|appendChild|text|post||type|src|head|feed|innerHTML|downPageWord|for|getElementsByTagName|setAttribute|start|index|halamanblogger|loophalaman|published|location|showpageOf|getElementsByName|openSearch|totalResults|pageArea|20|showpage|body|blog|pager|Page|html|showpagePoint|entry|of|23|29|encodeURIComponent|getElementById'.split('|'),0,{}))
+$('.post').each(function() {
+    if ($(this).find('.post-label').length) {     
+        var cl = $(this).find('.post-label').text();
+        $(this).find('.card-title').addClass(cl);
+		$(this).find('.card-content').addClass(cl);
+    }
+});
+var imgDefer=document.getElementsByTagName("img");for(var i=0;i<imgDefer.length;i++){if(imgDefer[i].getAttribute("data-src")){imgDefer[i].setAttribute("src",imgDefer[i].getAttribute("data-src"))}};
+function resizeThumb(parentID, size) {
+    var parent = document.getElementById(parentID),
+        image = parent.getElementsByTagName('img');
+        size2 = $('.post').width()
+    for (var i = 0; i < image.length; i++) {
+        image[i].src = image[i].src.replace("/s72-c/", "/w" + size + "-h" + size2 + "/");
+        image[i].width = size;
+        image[i].height = size2;
+    }
+}
+resizeThumb('Blog1', 345);
