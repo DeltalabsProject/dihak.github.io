931a77fe7a125331c93eb77be17903b636e391ee
Dihak <dihakoo@gmail.com>
2016-01-05 18:02:23 +0700

    Update home.js

--- bloggerku/home.js
+++ bloggerku/home.js
@@ -1,4 +1,203 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 N;5 n;5 i;5 v;1l();C j(a){5 b=A.15("E");b.1a="17/13";b.1b=a;A.1w.16(b)}C 1m(a){5 b=\'\';L=I(O/2);4(L==O-L){O=L*2+1}H=i-L;4(H<1)H=1;k=I(a/r)+1;4(k-1==a/r)k=k-1;K=H+O-1;4(K>k)K=k;b+="<3 6=\'1p\'>1z "+i+\' 1D \'+k+"</3>";5 c=I(i)-1;4(i>1){4(i==2){4(n=="u"){b+=\'<3 6="1v"><a 7="\'+D+\'">\'+X+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">\'+X+\'</a></3>\'}}9{4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+c+\');z B">\'+X+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+c+\');z B">\'+X+\'</a></3>\'}}}4(H>1){4(n=="u"){b+=\'<3 6="l"><a 7="\'+D+\'">1</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">1</a></3>\'}}1g(5 d=H;d<=K;d++){4(i==d){b+=\'<3 6="1B">\'+d+\'</3>\'}9 4(d==1){4(n=="u"){b+=\'<3 6="l"><a 7="\'+D+\'">1</a></3>\'}9{b+=\'<3 6="l"><a 7="/w/x/\'+v+\'?&h-o=\'+r+\'">1</a></3>\'}}9{4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+d+\');z B">\'+d+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+d+\');z B">\'+d+\'</a></3>\'}}}4(K<k){4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+k+\');z B">\'+k+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+k+\');z B">\'+k+\'</a></3>\'}}5 e=I(i)+1;4(i<k){4(n=="u"){b+=\'<3 6="l"><a 7="#" y="M(\'+e+\');z B">\'+1f+\'</a></3>\'}9{b+=\'<3 6="l"><a 7="#" y="J(\'+e+\');z B">\'+1f+\'</a></3>\'}}5 f=A.1q("1t");5 g=A.1H("1x-1y");1g(5 p=0;p<f.P;p++){f[p].1e=b}4(f&&f.P>0){b=\'\'}4(g){g.1e=b}}C 11(a){5 b=a.1d;5 c=I(b.1r$1s.$t,10);1m(c)}C 1l(){5 a=s;4(a.m("/w/x/")!=-1){4(a.m("?U-h")!=-1){v=a.F(a.m("/w/x/")+14,a.m("?U-h"))}9{v=a.F(a.m("/w/x/")+14)}}4(a.m("?q=")==-1&&a.m(".1A")==-1){4(a.m("/w/x/")==-1){n="u";4(s.m("#G=")!=-1){i=s.F(s.m("#G=")+8,s.P)}9{i=1}j(D+"Z/T/Y?h-o=1&W=R-Q-E&V=11")}9{n="x";4(a.m("&h-o=")==-1){r=1u}4(s.m("#G=")!=-1){i=s.F(s.m("#G=")+8,s.P)}9{i=1}j(D+\'Z/T/Y/-/\'+v+\'?W=R-Q-E&V=11&h-o=1\')}}}C M(a){S=(a-1)*r;N=a;5 b=A.1h(\'1c\')[0];5 c=A.15(\'E\');c.1a=\'17/13\';c.1i("1b",D+"Z/T/Y?1j-1k="+S+"&h-o=1&W=R-Q-E&V=12");b.16(c)}C J(a){S=(a-1)*r;N=a;5 b=A.1h(\'1c\')[0];5 c=A.15(\'E\');c.1a=\'17/13\';c.1i("1b",D+"Z/T/Y/-/"+v+"?1j-1k="+S+"&h-o=1&W=R-Q-E&V=12");b.16(c)}C 12(a){18=a.1d.1C[0];5 b=18.1n.$t.F(0,19)+18.1n.$t.F(1E,1F);5 c=1G(b);4(n=="u"){5 d="/w?U-h="+c+"&h-o="+r+"#G="+N}9{5 d="/w/x/"+v+"?U-h="+c+"&h-o="+r+"#G="+N}1o.7=d}',62,106,'|||span|if|var|class|href||else||||||||max|nomerhal||maksimal|showpageNum|indexOf|jenis|results|||postperpage|urlactivepage||page|lblname1|search|label|onclick|return|document|false|function|home_page|script|substring|PageNo|mulai|parseInt|redirectlabel|akhir|nomerkiri|redirectpage|nopage|numshowpage|length|in|json|jsonstart|posts|updated|callback|alt|upPageWord|summary|feeds||hitungtotaldata|finddatepost|javascript||createElement|appendChild|text|post||type|src|head|feed|innerHTML|downPageWord|for|getElementsByTagName|setAttribute|start|index|halamanblogger|loophalaman|published|location|showpageOf|getElementsByName|openSearch|totalResults|pageArea|20|showpage|body|blog|pager|Page|html|showpagePoint|entry|of|23|29|encodeURIComponent|getElementById'.split('|'),0,{}))
+var nopage;
+var jenis;
+var nomerhal;
+var lblname1;
+halamanblogger();
+
+function j(a) {
+    var b = document.createElement("script");
+    b.type = "text/javascript";
+    b.src = a;
+    document.body.appendChild(b)
+}
+
+function loophalaman(a) {
+    var b = '';
+    nomerkiri = parseInt(numshowpage / 2);
+    if (nomerkiri == numshowpage - nomerkiri) {
+        numshowpage = nomerkiri * 2 + 1
+    }
+    mulai = nomerhal - nomerkiri;
+    if (mulai < 1) mulai = 1;
+    maksimal = parseInt(a / postperpage) + 1;
+    if (maksimal - 1 == a / postperpage) maksimal = maksimal - 1;
+    akhir = mulai + numshowpage - 1;
+    if (akhir > maksimal) akhir = maksimal;
+    var c = parseInt(nomerhal) - 1;
+    if (nomerhal == 1) {
+        if (jenis == "page") {
+            b += '<span class="showpage"><a href="' + home_page + '">' +
+                upPageWord + '</a></span>'
+        } else {
+            b += '<span class="showpageNum"><a href="/search/label/' +
+                lblname1 + '?&max-results=' + postperpage + '">' +
+                upPageWord + '</a></span>'
+        }
+    } else {
+        if (jenis == "page") {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectpage(' +
+                c + ');return false">' + upPageWord + '</a></span>'
+        } else {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectlabel(' +
+                c + ');return false">' + upPageWord + '</a></span>'
+        }
+    }
+    if (mulai > 1) {
+        if (jenis == "page") {
+            b += '<span class="showpageNum"><a href="' + home_page +
+                '">1</a></span>'
+        } else {
+            b += '<span class="showpageNum"><a href="/search/label/' +
+                lblname1 + '?&max-results=' + postperpage +
+                '">1</a></span>'
+        }
+    }
+    for (var d = mulai; d <= akhir; d++) {
+        if (nomerhal == d) {
+            b += '<span class="showpagePoint">' + d + '</span>'
+        } else if (d == 1) {
+            if (jenis == "page") {
+                b += '<span class="showpageNum"><a href="' + home_page +
+                    '">1</a></span>'
+            } else {
+                b += '<span class="showpageNum"><a href="/search/label/' +
+                    lblname1 + '?&max-results=' + postperpage +
+                    '">1</a></span>'
+            }
+        } else {
+            if (jenis == "page") {
+                b +=
+                    '<span class="showpageNum"><a href="#" onclick="redirectpage(' +
+                    d + ');return false">' + d + '</a></span>'
+            } else {
+                b +=
+                    '<span class="showpageNum"><a href="#" onclick="redirectlabel(' +
+                    d + ');return false">' + d + '</a></span>'
+            }
+        }
+    }
+    if (akhir < maksimal) {
+        if (jenis == "page") {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectpage(' +
+                maksimal + ');return false">' + maksimal + '</a></span>'
+        } else {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectlabel(' +
+                maksimal + ');return false">' + maksimal + '</a></span>'
+        }
+    }
+    var e = parseInt(nomerhal) + 1;
+    if (nomerhal < maksimal) {
+        if (jenis == "page") {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectpage(' +
+                e + ');return false">' + downPageWord + '</a></span>'
+        } else {
+            b +=
+                '<span class="showpageNum"><a href="#" onclick="redirectlabel(' +
+                e + ');return false">' + downPageWord + '</a></span>'
+        }
+    }
+    var f = document.getElementsByName("pageArea");
+    var g = document.getElementById("blog-pager");
+    for (var p = 0; p < f.length; p++) {
+        f[p].innerHTML = b
+    }
+    if (f && f.length > 0) {
+        b = ''
+    }
+    if (g) {
+        g.innerHTML = b
+    }
+}
+
+function hitungtotaldata(a) {
+    var b = a.feed;
+    var c = parseInt(b.openSearch$totalResults.$t, 10);
+    loophalaman(c)
+}
+
+function halamanblogger() {
+    var a = urlactivepage;
+    if (a.indexOf("/search/label/") != -1) {
+        if (a.indexOf("?updated-max") != -1) {
+            lblname1 = a.substring(a.indexOf("/search/label/") + 14, a.indexOf(
+                "?updated-max"))
+        } else {
+            lblname1 = a.substring(a.indexOf("/search/label/") + 14)
+        }
+    }
+    if (a.indexOf("?q=") == -1 && a.indexOf(".html") == -1) {
+        if (a.indexOf("/search/label/") == -1) {
+            jenis = "page";
+            if (urlactivepage.indexOf("#PageNo=") != -1) {
+                nomerhal = urlactivepage.substring(urlactivepage.indexOf(
+                    "#PageNo=") + 8, urlactivepage.length)
+            } else {
+                nomerhal = 1
+            }
+            j(home_page +
+                "feeds/posts/summary?max-results=1&alt=json-in-script&callback=hitungtotaldata"
+            )
+        } else {
+            jenis = "label";
+            if (a.indexOf("&max-results=") == -1) {
+                postperpage = 20
+            }
+            if (urlactivepage.indexOf("#PageNo=") != -1) {
+                nomerhal = urlactivepage.substring(urlactivepage.indexOf(
+                    "#PageNo=") + 8, urlactivepage.length)
+            } else {
+                nomerhal = 1
+            }
+            j(home_page + 'feeds/posts/summary/-/' + lblname1 +
+                '?alt=json-in-script&callback=hitungtotaldata&max-results=1'
+            )
+        }
+    }
+}
+
+function redirectpage(a) {
+    jsonstart = (a - 1) * postperpage;
+    nopage = a;
+    var b = document.getElementsByTagName('head')[0];
+    var c = document.createElement('script');
+    c.type = 'text/javascript';
+    c.setAttribute("src", home_page + "feeds/posts/summary?start-index=" +
+        jsonstart +
+        "&max-results=1&alt=json-in-script&callback=finddatepost");
+    b.appendChild(c)
+}
+
+function redirectlabel(a) {
+    jsonstart = (a - 1) * postperpage;
+    nopage = a;
+    var b = document.getElementsByTagName('head')[0];
+    var c = document.createElement('script');
+    c.type = 'text/javascript';
+    c.setAttribute("src", home_page + "feeds/posts/summary/-/" + lblname1 +
+        "?start-index=" + jsonstart +
+        "&max-results=1&alt=json-in-script&callback=finddatepost");
+    b.appendChild(c)
+}
+
+function finddatepost(a) {
+    post = a.feed.entry[0];
+    var b = post.published.$t.substring(0, 19) + post.published.$t.substring(
+        23, 29);
+    var c = encodeURIComponent(b);
+    if (jenis == "page") {
+        var d = "/search?updated-max=" + c + "&max-results=" + postperpage +
+            "#PageNo=" + nopage
+    } else {
+        var d = "/search/label/" + lblname1 + "?updated-max=" + c +
+            "&max-results=" + postperpage + "#PageNo=" + nopage
+    }
+    location.href = d
+}
 $('.post').each(function() {
     if ($(this).find('.post-label').length) {     
         var cl = $(this).find('.post-label').text();
