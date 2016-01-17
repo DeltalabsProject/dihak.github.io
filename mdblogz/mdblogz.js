69d1fafc972f878e88d691289b9bd587def076c8
Dihak <dihakoo@gmail.com>
2016-01-17 18:14:18 +0700

    Update mdblogz.js

--- mdblogz/mdblogz.js
+++ mdblogz/mdblogz.js
@@ -7,14 +7,13 @@ $('.post').each(function() {
     }
 });
 var imgDefer=document.getElementsByTagName("img");for(var i=0;i<imgDefer.length;i++){if(imgDefer[i].getAttribute("data-src")){imgDefer[i].setAttribute("src",imgDefer[i].getAttribute("data-src"))}};
-function resizeThumb(parentID, size) {
+function resizeThumb(parentID, size, size2) {
     var parent = document.getElementById(parentID),
         image = parent.getElementsByTagName('img');
-        size2 = $('.post').width()
     for (var i = 0; i < image.length; i++) {
         image[i].src = image[i].src.replace("/s72-c/", "/w" + size + "-h" + size2 + "/");
         image[i].width = size;
         image[i].height = size2;
     }
 }
-resizeThumb('Blog1', 345);
+resizeThumb('Blog1', 345, 500);
